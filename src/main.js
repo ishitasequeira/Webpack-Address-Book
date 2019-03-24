bd6dde4c927275d840ff381036d8581aac6b5df7
sequeira_ishita_001403357 <sequeira.i@husky.neu.edu>
2019-03-24 14:15:47 -0400

    added addcontact logic in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
  import './../scss/main.scss';  
 
-import { Observable, fromEvent, combineLatest, zip } from 'rxjs'; 
+import { Observable, fromEvent,fromPromise} from 'rxjs/Rx'; 
+
 // import { map, debounceTime } from 'rxjs/operators';
 
 var modal = document.getElementById("addContactModal");
 let button = document.getElementById('addContact');
-let buttonClick$ = fromEvent(button, 'click');
+let buttonClick$ = Observable.fromEvent(button, 'click');
 buttonClick$.subscribe(x => modal.style.display="block");
 
 
@@ -15,3 +16,21 @@ window.addEventListener('click', (event) => {
         modal.style.display="none";
     }
 });
+
+//retreiving data from server using fetchapi
+const fetch$ = Observable
+  .from(fetch("http://localhost:3000/contacts"))
+  .flatMap(response => response.json());
+    fetch$.subscribe(x=>{
+        for(var i = 0; i<x.length;i++){
+        const row = document.createElement("li");
+        row.innerHTML=x[i].FirstName + " " + x[i].LastName;
+        document.getElementById("list").append(row);
+        console.log(x[i]);
+    }
+    });
+// var result = fromPromise(fetch("http://localhost:3000/contacts"));
+// result.subscribe(x=>{
+//     // document.createElement("li").innerHTML=x.first
+//     alert(x.getBody());
+// })
